57163e886e838e4e5df5ba3bd58f21d02b1d6e52
Vimlesh <pandeyvimleshkumar@gmail.com>
2023-01-28 01:04:07 +0530

    Updated csrf trusted origins

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -133,4 +133,4 @@ MEDIA_ROOT= BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL='account.MyRegistration'
-CSRF_TRUSTED_ORIGINS = ['https://optimuscore-production.up.railway.app','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://optimuscore.up.railway.app','https://*.127.0.0.1']
