3d4bbef2eb7009340063814cd71f517a382e3a97
Vimlesh <pandeyvimleshkumar@gmail.com>
2023-01-27 23:49:46 +0530

    Ready after creating static folder in base directory

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR/'staticfiles'
-# STATICFILES_DIRS=[BASE_DIR/'static',]
+STATICFILES_DIRS=[BASE_DIR/'static',]
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR/'media'
 
