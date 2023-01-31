a7ca218a5ad25c0506dc3c7461383cfb57684fa7
Vimlesh <pandeyvimleshkumar@gmail.com>
2023-01-31 16:25:41 +0530

    Updated secret key and gitignore

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wl5#n_b(f010p_k2jbre#scl6iugxu4li2h!b0h6mfvcb#10mr'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
